69b9b80067db9480d868e35fc8f41492f487d892
aafearhmn <155437656+aafearhmn@users.noreply.github.com>
2024-05-31 07:03:03 +0800

    Delete views.py

--- views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import MyModel
-
-def my_view(request):
-    objects = MyModel.objects.all() 
-    return render(request, 'myapp/my_template.html', {'objects': objects})

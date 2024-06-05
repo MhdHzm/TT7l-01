baaf12c03494c0059dbde2a98a2bf5d1fcdf4e1d
aafearhmn <155437656+aafearhmn@users.noreply.github.com>
2024-06-05 12:50:57 +0800

    Create models.py

---
+++ users/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.uth.models import User
+
+class Users(models.Model):
+  title = models.CharField(max_length=200)
+  description = models.TextField()
+  date = models.DateField()
+  time = models.TimeField()
+  location = models.CharField(max_length=200)
+  organizer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  def __str__(self):
+      return self.title
+
+class Attendee (models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+def __str__(self):
+  return f"{self.user.username} - {self.event.title}"

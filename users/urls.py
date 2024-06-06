a2d02bfa8c78f50a0f01d3a2173f20241d74e81b
aafearhmn <155437656+aafearhmn@users.noreply.github.com>
2024-06-07 00:42:47 +0800

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = 'users'
-
 urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('settings/',views.settings, name='settings'),

66d6311a3efe382e5dbc6e175acc3c2ffe350320
aafearhmn <155437656+aafearhmn@users.noreply.github.com>
2024-06-06 22:21:27 +0800

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')), 
-    path('', include('users.urls')),  
+    path('myadmin/', admin.site.urls),
+    path('myaccounts/', include('allauth.urls')), 
+    path('', include('users.urls',namespace='users')),  
 ]

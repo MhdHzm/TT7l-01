9ed0c20af396f7faee608d98588a39df58d7d4c1
aafearhmn <155437656+aafearhmn@users.noreply.github.com>
2024-06-06 23:44:39 +0800

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
 @login_required
 def home(request):
-    return HttpResponse ("Welcome to Online Event Ticket Booking System!") 
+    return HttpResponse ("Welcome to Online Event Ticket Booking System!")
+
+def profile(request):
+    return render(request, 'profile.html') 

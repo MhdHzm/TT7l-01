1927c035211f76d00c4d6d964ce705d75d0c402e
aafearhmn <155437656+aafearhmn@users.noreply.github.com>
2024-06-14 03:01:52 +0800

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,28 +1,23 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-class Event(models.Model):
-  event_type = models.CharField(max_length=200)
-  venue = models.TextField()
-  date = models.DateField()
-  time = models.TimeField()
-  location = models.CharField(max_length=200)
 
-  def __str__(self):
-      return f"{self.event_type} at {self.venue} on {self.date}"
+PRIORITY = [
+    ("H", "High"),
+    ("M", "Medium"),
+    ("L", "Low"),
+]
 
-class Registration (models.Model):
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-  event = models.ForeignKey(Event, on_delete=models.CASCADE)
-  registration_date = models.DateTimeField(auto_now_add=True)
+class Question(models.Model):
+    title                   = models.CharField(max_length=60)
+    question                = models.TextField(max_length=400)
+    priority                = models.CharField(max_length=1, choices=PRIORITY)
 
-  def __str__(self):
-    return f"{self.user.username} - {self.event.title}"
-
-class Ticket(models.Model):
-  event = models.ForeignKey(Event, on_delete=models.CASCADE)
-  ticket_type = models.CharField(max_length=50)
-  price = models.DecimalField(max_digits=10, decimal_places=2)
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "The Question"
+        verbose_name_plural = "Peoples Questions"
   quantity = models.IntegerField()
 
   def __str__(self):

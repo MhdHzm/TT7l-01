1a058aee26f1781531a8f1cb0ffadf90dd221054
aafearhmn <155437656+aafearhmn@users.noreply.github.com>
2024-06-14 01:42:50 +0800

    Update views.py

--- users/views.py
+++ users/views.py
@@ -2,13 +2,20 @@ from django.shortcuts import render
 
 def home_screen_view(request):
     
-    # context = {}
+    context = {}
     # context['some_string'] = "this is some string that I'm passing to the view"
     # context['some_number'] = 141251
 
-    context = {
-        'some_string':"this is some string that I'm passing to the view",
-        'some_number' : 124125,
-    }
+    # context = {
+    #     'some_string':"this is some string that I'm passing to the view",
+    #     'some_number' : 124125,
+    # }
+
+    list_of_values = []
+    list_of_values.append("first entry")
+    list_of_values.append("second entry")
+    list_of_values.append("third entry")
+    list_of_values.append("fourth entry")
+    context['list_of_values'] = list_of_values
     
     return render(request, "project/home.html", {})

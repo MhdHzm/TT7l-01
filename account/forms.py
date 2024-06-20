703176d6bb6b7b8daf6eed2336c5cc79e857f2ad
aafearhmn <155437656+aafearhmn@users.noreply.github.com>
2024-06-21 04:47:49 +0800

    Update forms.py

--- account/forms.py
+++ account/forms.py
@@ -4,4 +4,8 @@ from django.contrib.auth.forms import UserCreationForm
 from account.models import Account
 
 class RegistrationForm(UserCreationForm):
-    email = forms.EmailField(max_length=70)
+    email = forms.EmailField(max_length=70,help_text='Required.Add a valid email address')
+
+    class Meta:
+        model = Account
+        fields = ("email", "username", "password", "password2"))

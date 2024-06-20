12a3d8c5406cda48313257289a58f05aebbec76c
aafearhmn <155437656+aafearhmn@users.noreply.github.com>
2024-06-21 04:44:42 +0800

    Update forms.py

--- account/forms.py
+++ account/forms.py
@@ -1,1 +1,7 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
 
+from account.models import Account
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(max_length=70)
